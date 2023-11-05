d2d8c936523f90b9c8bf00f9e15a86a27aeadbac
nkchambers <nkchames@gmail.com>
2023-11-04 17:24:01 -0700

    modofied cors origin public dns - aws > linode

--- server.js
+++ server.js
@@ -39,8 +39,12 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
     credentials: true,
+    // Localhost 
     // origin: 'http://localhost:5173'
-    origin: 'http://52.10.71.141/'
+    // AWS EC2
+    // origin: 'http://52.10.71.141/'
+    // Linode
+    origin: 'http://172.232.170.81/'
 }));
 
 

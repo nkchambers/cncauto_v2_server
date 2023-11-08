961c8dcabc82e301127d7a63f8a28c8c66e65d65
nkchambers <nkchames@gmail.com>
2023-11-08 15:22:56 -0800

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 //-------------------------- IMPORTS NEEDED ----------------------------
-// IMPORT Express - call it express b/c official documentation says so 
+// IMPORT Express - call it express b/c official documentation says so
 // >>> require express from node_modules
 const express = require('express');
 
@@ -42,11 +42,11 @@ app.use('/uploads', express.static(__dirname + '/uploads'));
 
 app.use(cors({
     credentials: true,
-    // Localhost 
-    origin: 'http://localhost:5173'
+    // Localhost
+    // origin: 'http://localhost:5173'
     // origin: '*'
     // AWS EC2
-    // origin: 'http://52.10.71.141/'
+    origin: 'http://52.10.71.141/'
     // Linode
     // origin: 'http://172.232.170.81/'
 }));
